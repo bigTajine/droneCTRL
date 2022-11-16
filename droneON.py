9798dcba5bd0c3027d9e173a97d129db15ac30f5
DrHouseOfComputers <112186613+DrHouseOfComputers@users.noreply.github.com>
2022-11-16 17:35:51 +0000

    changing text file to pull information from

--- droneON.py
+++ droneON.py
@@ -1,13 +1,8 @@
 def switchFunc(version):
-    with open(version, 'r') as file:
-        filedata = file.read()
-    if filedata == 'ON':
-        filedata = filedata.replace('ON', 'OFF')
-    elif filedata == 'OFF':
-        filedata = filedata.replace('OFF', 'ON')
-    with open(version, 'w') as file:
-        file.write(filedata)
+    search_On = 'ON'
+    search_OFF = 'OFF'
+    try:
+        with open(version, 'r') as file:
+            for index, line in enumerate(file):
+                if 'ON' in line:
 
-
-if __name__ == '__main__':
-    switchFunc('drone.txt')
