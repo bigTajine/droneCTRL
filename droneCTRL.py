e59f7379d95caf12142388550e2f1748a19fd1fe
DrHouseOfComputers <112186613+DrHouseOfComputers@users.noreply.github.com>
2022-11-16 17:12:43 +0000

    New Function for Version Control

--- droneCTRL.py
+++ droneCTRL.py
@@ -18,7 +18,8 @@ import x_pos_set
 import bat_set
 import x_pos_inc
 import bat_dec
-
+from version_control import *
+from droneON import *
 '''
 Initialization
 '''
@@ -39,7 +40,7 @@ Start of the Drone Controller
 '''
 
 print("──────────────────")
-print("@ droneCTRL v" + str(version_number) + random.choice(kawaii_list))
+print("@ droneCTRL v" +  + random.choice(kawaii_list))
 print("$", datetime.now())
 print("──────────────────")
 
