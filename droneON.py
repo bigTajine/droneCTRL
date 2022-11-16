28eb375925121e3515d266d506b43fee012e9868
DrHouseOfComputers <112186613+DrHouseOfComputers@users.noreply.github.com>
2022-11-16 17:03:09 +0000

    Reworked drone states

--- droneON.py
+++ droneON.py
@@ -1,10 +1,12 @@
-# Read in the file
-with open('drone.txt', 'r') as file :
-  filedata = file.read()
+def switchFunc(version):
+    with open(version, 'r') as file:
+      filedata = file.read()
+    if filedata == 'ON':
+      filedata = filedata.replace('ON', 'OFF')
+    elif filedata == 'OFF':
+      filedata = filedata.replace('OFF', 'ON')
+    with open(version, 'w') as file:
+      file.write(filedata)
 
-# Replace the target string
-filedata = filedata.replace('0', '1')
-
-# Write the file out again
-with open('drone.txt', 'w') as file:
-  file.write(filedata)
+if __name__ == '__main__':
+  switchFunc('drone.txt')
