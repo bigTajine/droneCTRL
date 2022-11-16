c558abebe29cd85bfd0835a637262fb73536ad23
DrHouseOfComputers <112186613+DrHouseOfComputers@users.noreply.github.com>
2022-11-16 17:05:50 +0000

    New Function for Version Control

---
+++ version_control.py
@@ -0,0 +1,15 @@
+def Information(version):
+    version_control = ''
+    try:
+        with open(version, 'r') as file:
+            for index, line in enumerate(file):
+                if 'version' in line:
+                    version_control = line
+                    break
+    except Exception:
+        version_control = 'version 1.0'
+
+    return version_control
+
+
+print(Information('drone.txt'))
